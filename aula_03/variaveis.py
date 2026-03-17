429c32d6211b7f49d339f3c651f26c816aa9bbe8
Barbara <barbaravarelabonfim@gmail.com>
2026-03-17 09:31:51 -0300

    Subindo os primeiros arquivos

---
+++ aula_03/variaveis.py
@@ -0,0 +1,13 @@
+# Variáveis e Tipos de Dados:
+# Em python, você não precsa declarar o tipo de uma varáel antes de usá-la. O tipo é inferido automaticamente pelo interpretador.
+# Em Python, a variável é declarada no padrão snake_case.
+# 1. string ->
+# 2. number - int (número inteiro) ->
+# 3. number - float (número decimal) ->
+# 4. boolen - True ou False (verdadeiro ou falso) ->
+nome_usuario = "Barbara" # string # NÃO usar do, da, de, ...
+email = "barbaravarelabonfim@gmail.com" # string
+cidade = "Rio de Janeiro" # string
+cpf = 13055991796 # number-int
+salario = 14000.65 # number-float
+casado = False # boolean
